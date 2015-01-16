b19fd0ec96218011c643610a52fdfdc4b3039486
Kevin M. Godby <kevin@godby.org>
2015-01-16 17:57:47 -0600

    Adding Oculus Runtime for Windows package.

---
+++ oculus-runtime/oculus-runtime.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿
+$packageName = 'oculus-runtime.install'
+$installerType = 'exe'
+$url = 'http://static.oculus.com/sdk-downloads/oculus_runtime_rev_1_sdk_0.4.4_win.exe'
+$silentArgs = '--mode unattended'
+$checksum = 'e1d5c9e3d39f15dfe4cd2d992a492cf6'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType"
