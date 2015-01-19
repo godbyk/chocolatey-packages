11af87241d8ebf2826cd45dc5e7eafba27244821
Ryan Pavlik <ryan@sensics.com>
2015-01-19 11:25:56 -0600

    Add a little build script.

---
+++ build-all.ps1
@@ -0,0 +1,8 @@
+﻿$OrigDir = $PWD
+$ScriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+Set-Location $ScriptPath
+foreach ($nuspec in @(Get-ChildItem -path "$PWD\*.nuspec" -recurse)) {
+    Set-Location $nuspec.Directory.Parent.FullName
+    nuget pack (join-path $nuspec.Directory $nuspec.Name) -NoPackageAnalysis -NonInteractive
+}
+Set-Location $OrigDir
